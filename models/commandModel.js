0fa43d6a666170db61a7fc3c493a2787b2ab0f20
Gauthier <bosson.gauthier27@gmail.com>
2020-01-28 14:52:06 +0100

    Added pre populate in command for PDF gen

--- models/commandModel.js
+++ models/commandModel.js
@@ -5,6 +5,7 @@ const commandSchema = new mongoose.Schema({
     {
         prestation : {
             type: mongoose.Types.ObjectId,
+            ref: 'Prestation',
             required: true
         },
         number: {
@@ -15,8 +16,19 @@ const commandSchema = new mongoose.Schema({
   ],
   client: {
     type: mongoose.Types.ObjectId,
+    ref: 'Client',
     required: true
   }
+});
+
+commandSchema.pre('find', function() {
+  this.populate('prestations.prestation');
+  this.populate('client')
+});
+
+commandSchema.pre('findOne', function() {
+  this.populate('prestations.prestation');
+  this.populate('client')
 });
 
 const Command = mongoose.model('Command', commandSchema);
