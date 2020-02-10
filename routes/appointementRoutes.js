808737acd636901bd68de9b5e6718708a577a70c
Gauthier <bosson.gauthier27@gmail.com>
2020-02-11 00:07:09 +0100

    added appointement model + some routes

--- routes/appointementRoutes.js
+++ routes/appointementRoutes.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const appointementController = require('../controllers/appointementController');
+const authController = require('../controllers/authController');
+
+router.use(authController.protect);
 
 router
   .route('/')
+  .get(appointementController.getAllAppointements)
+
+router
+  .route('/:id')
+  .get(appointementController.getOneAppointement)
+
+router
+  .route('/confirm/:id')
+  .post(appointementController.confirmAppointement)
+
+router
+  .route('/generateIcs')
   .post(appointementController.createEvents)
 
 module.exports = router;
