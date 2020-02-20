577dda22a7d8aa9cb4c248aabe7c6cdb11d749ee
Gauthier <bosson.gauthier27@gmail.com>
2020-02-20 14:58:00 +0100

    update server

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(database, {
 });
 
 // Start the server
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Application is running on port ${port}`);
 });
